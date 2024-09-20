f124eb4110f9aae9728aaf7468f43061758bac2a
Larilie <leandrodailoff@gmail.com>
2024-09-20 16:19:05 -0300

    Creacion de "Mi Cuenta"

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,10 +1,11 @@
-import { SafeAreaView, Text } from "react-native";
 import React from "react";
+import { View, Text } from "react-native";
+import LoginForm from "../components/Auth/LoginForm";
+import UserData from "../components/Auth/UserData";
+import useAuth from "../hooks/useAuth";
 
 export default function Account() {
-  return (
-    <SafeAreaView>
-      <Text>Mi cuenta</Text>
-    </SafeAreaView>
-  );
+  const { auth } = useAuth();
+
+  return <View>{auth ? <UserData /> : <LoginForm />}</View>;
 }
---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: "lea",
+  password: "123456",
+};
+
+export const userDetails = {
+  username: "lea",
+  firstName: "Leandro",
+  lastName: "Dailoff",
+  email: "leandrodailoff@gmail.com",
+};
