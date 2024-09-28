88b56055e45c3e604b85f39fe2c80f68a1e8a2d8
Leandro Dailoff <leandrodailoff@outlook.com>
2024-09-28 18:09:15 -0300

    test

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigation from "./src/navigation/Navigation";
 import { AuthProvider } from "./src/context/AuthContext";
-
+console.log("Hola");
 export default function App() {
   return (
     <NavigationContainer>
